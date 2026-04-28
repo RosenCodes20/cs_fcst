5663eae650f17d5c99fad335f36c66745ffad2d8
Rosen Ivanov <rrirrirri08@gmail.com>
2026-04-28 21:07:23 +0300

    feat: hid the info about the db;

--- system/_config.php
+++ system/_config.php
@@ -8,7 +8,30 @@
     // !important - Other configs:
 // - .htaccess
 
+    function loadEnv($path) {
+        if (!file_exists($path)) return;
 
+        $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            if (strpos(trim($line), '#') === 0) continue;
+            list($name, $value) = explode('=', $line, 2);
+
+            $name = trim($name);
+            $value = trim($value);
+
+            if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
+                putenv(sprintf('%s=%s', $name, $value));
+                $_ENV[$name] = $value;
+            }
+        }
+    }
+
+    loadEnv($_SERVER['DOCUMENT_ROOT'] . '/.env');
+
+    define('SQL_HOST', getenv('SQL_HOST'));
+    define('SQL_DATABASE', getenv('SQL_DATABASE'));
+    define('SQL_USER', getenv('SQL_USER'));
+    define('SQL_PASSWORD', getenv('SQL_PASSWORD'));
 
 // �����
 
